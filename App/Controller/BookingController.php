9630e16076c2fa8a104574d7f8e7de5ceff79208
unknown <>
2021-03-02 17:20:48 +0100

    reservation table ok

---
+++ App/Controller/BookingController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+
+use App\Model\BookingModel;
+use Vendor\Controller\Controller;
+require('../inc/functions.php');
+
+/**
+ * Le CRUD (create, read, update, delete) sont l'ensemble des méthodes permettant de manipuler
+ * des données dans la base de données.
+ */
+class BookingController extends Controller{
+
+
+    public function booking()
+    {
+        $bookingModel = new BookingModel();
+        $error = [];
+
+        if (isset($_POST["submitted"])) {
+            // $user = $this->encodeChars($_POST);
+            $booking["BookingDate"] = $_POST["date"];
+            $booking['BookingTime'] = $_POST['time'];
+            $booking['NumberOfSeats'] = $_POST['tablenumb'];
+            $booking['User_Id'] = 16;
+            $error = emptyError($error,$_POST['date'],'date');
+            $error = emptyError($error,$_POST['time'],'time');
+            $error = emptyError($error,$_POST['tablenumb'],'tablenumb');
+
+
+            if (empty($error)) {
+              $bookingModel->create($booking);
+            }
+
+
+
+        }
+        $this->render("booking", [
+            "error" => $error,
+            ]);
+
+    }
+
+}
---
+++ App/Model/BookingModel.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Model;
+
+use Vendor\Model\Query;
+
+/**
+ * @method ReadAll() | Récupère tous les catégories
+ * @method ReadOne(int $id) | Retourne une catégorie en fonction de son id
+ * @method delete(int $id) | Supprime une catégorie en fonction de son id
+ * @method create($data) | Enregistre une catégorie dans la BDD
+ */
+class BookingModel extends Query{
+
+    /**
+     * Nom de la table
+     *
+     * @var string
+     */
+    protected $table = "booking";
+
+    /**
+     * Met à jour une catégorie dans la BDD
+     *
+     * @param int $id
+     * @param array $data
+     */
+    public function update(int $id, array $data)
+    {
+        $statement = "UPDATE $this->table SET
+                        Name = :Name
+                        WHERE id = $id";
+
+        $this->db->postData($statement, $data);
+    }
+}
