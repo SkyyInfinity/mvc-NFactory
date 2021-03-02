77420a0f8e54700775292f4caf9db0a0a4c81db3
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2021-03-02 14:38:19 +0100

    modification de la page d'accueil (style + structure + contenu)

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,6 +1,21 @@
 <?php
 $title = 'Home';
+?>
 
-foreach ($meals as $meal) {
-  echo $meal->Name;
-}
+<div class="wrap2">
+  <h2>Carte du restaurant</h2>
+  <ul class="meals-links">
+    <?php foreach($meals as $meal) : ?>
+      <li>
+        <div class="img">
+          <img loading="lazy" src="../assets/img/meals/<?php if(isset($meal->Photo)){echo $meal->Photo;}else{echo 'no-photo.png';} ?>" alt="<?php if(isset($meal->Name)){echo $meal->Name;} ?>">
+        </div>
+        <div class="info">
+          <h3><?php if(isset($meal->Name)){echo $meal->Name;} ?></h3>
+          <p><?php if(isset($meal->Description)){echo $meal->Description;} ?></p>
+          <span class="price"><?php if(isset($meal->SalePrice)){echo $meal->SalePrice;} ?>€</span>
+        </div>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
