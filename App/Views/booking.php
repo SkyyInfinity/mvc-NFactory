5270a652aa42951321e9fd44ac7451ea8d3faa21
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2021-03-03 16:24:16 +0100

    Ajout d'une option sur le select pour pouvoir afficher les erreurs

--- App/Views/booking.php
+++ App/Views/booking.php
@@ -12,10 +12,14 @@ $title = 'Réserver';
         <input type="date" name="date" value="<?php if(isset($_POST['date'])){echo $_POST['date'];} ?>">
         <span> à </span>
         <input class="date" type="time" name="time" value="<?php if(isset($_POST['time'])){echo $_POST['time'];} ?>">
+        <?php if (isset($error['time'])) : ?>
+                    <p style="color:red"> <?= $error['time']; ?></p>
+        <?php endif; ?>
       </div>
       <div class="champ">
         <label for="tablenumb">Nombre de personnes</label>
         <select id="tablenumb" name="tablenumb">
+          <option value="">--Veuillez choisir une option--</option>
           <option value="1">1 personne</option>
           <option value="2">2 personnes</option>
           <option value="3">3 personnes</option>
@@ -23,6 +27,9 @@ $title = 'Réserver';
           <option value="5">5 personnes</option>
           <option value="6">6 personnes</option>
         </select>
+        <?php if (isset($error['tablenumb'])) : ?>
+                    <p style="color:red"> <?= $error['tablenumb']; ?></p>
+        <?php endif; ?>
       </div>
     </fieldset>
     <div class="champ">
